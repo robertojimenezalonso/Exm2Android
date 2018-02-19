065a225e0486c136474f02871f65d145aeefd533
Roberto <robertojimenezalonso@gmail.com>
2018-02-19 11:12:40 +0100

    Derfinimos los elementos visuales fb button

--- app/src/main/java/com/example/roberto/exm2android/MainActivity.java
+++ app/src/main/java/com/example/roberto/exm2android/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.roberto.exm2android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+     LoginButton loginButton;
 
 
 
@@ -12,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 }
