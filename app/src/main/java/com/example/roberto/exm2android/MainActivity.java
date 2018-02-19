e23462f601f7b5d9310f1ad952b276eadfab63d2
Roberto <robertojimenezalonso@gmail.com>
2018-02-19 11:27:44 +0100

    visual loging fb y google, registerCallback handleFacebookAcessToken

--- app/src/main/java/com/example/roberto/exm2android/MainActivity.java
+++ app/src/main/java/com/example/roberto/exm2android/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.roberto.exm2android;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
 
 
 
@@ -19,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+               
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+         
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+      
+            }
+        });
+
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
 
     }
 
